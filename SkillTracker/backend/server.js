e9238ead2b9334b25c505cf2893b217819aa54f2
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-02 21:04:15 +0530

    Update for deployment

--- SkillTracker/backend/server.js
+++ SkillTracker/backend/server.js
@@ -27,18 +27,18 @@ app.use('/api/v1/quiz', require("./routes/quizRoute"));
 // ------------------------- Deployment Setup -------------------------
 const __dirname1 = path.resolve();
 
-// Serve frontend (React build)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
     });
 } else {
     app.get("/", (req, res) => {
         res.send("API is running...");
     });
 }
+
 // ---------------------------------------------------------------------
 
 
