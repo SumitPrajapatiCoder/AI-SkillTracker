1064044c0ace1683311022546a7671e565858cd7
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-02 21:47:05 +0530

    Update for deployment

--- SkillTracker/backend/server.js
+++ SkillTracker/backend/server.js
@@ -1,13 +1,11 @@
 const dotenv = require("dotenv");
-// dotenv.config();  
+dotenv.config();  
 
 const express = require("express");
 const morgan = require("morgan");
 const connect_DB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
-
-dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 connect_DB();
 const app = express();
