9f7608e0141175b1357cccf3f13b2e1218b687ab
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-02 21:32:15 +0530

    Update for deployment

--- SkillTracker/backend/config/db.js
+++ SkillTracker/backend/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
 const connect_DB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Database connected successfully on host: ${mongoose.connection.host}`.bgGreen.white);
+        console.log(`Database connected successfully on host: ${mongoose.connection.host}`);
     } catch (error) {
-        console.error(`MongoDB Connection Error: ${error.message}`.bgRed.white);
+        console.error(`MongoDB Connection Error: ${error.message}`);
     }
 };
 
--- SkillTracker/backend/server.js
+++ SkillTracker/backend/server.js
@@ -2,7 +2,6 @@ const dotenv = require("dotenv");
 // dotenv.config();  
 
 const express = require("express");
-const colors = require("colors");
 const morgan = require("morgan");
 const connect_DB = require("./config/db");
 const cors = require("cors");
@@ -45,6 +44,7 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server Running In ${process.env.NODE_ENV} Mode on Port ${port}`.bgCyan.white);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
+
 
