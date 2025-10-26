4a55d804ed406dd630cee5120951a44ac217c215
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-26 19:11:39 +0530

    Ready for Render deployment

--- SkillTracker/frontend/vite.config.js
+++ SkillTracker/frontend/vite.config.js
@@ -20,18 +20,18 @@
 
 
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080' 
-    }
+      '/api': 'http://localhost:8080', 
+    },
+    port: 3000,
   },
   build: {
     outDir: 'dist',
-  }
-})
+  },
+});
